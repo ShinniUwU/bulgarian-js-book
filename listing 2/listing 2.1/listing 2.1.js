a525865f465dc26f5bed792a33e3acbaba92066f
Shinni <69644134+ShinniUwU@users.noreply.github.com>
2022-02-12 17:50:00 +0200

    Delete listing 2 directory

--- listing 2/listing 2.1/listing 2.1.js
+++
@@ -1,9 +0,0 @@
-var text,name
-text ="Haide da se zapoznaem! Kak se kazvate?"
-name=prompt(text)
-if(name=="") {
-    document.write("vie ne se predstavihte")
-}
-else {
-document.write("Priqtno mi e da se zapoznaem, "+name+"!")
-}
