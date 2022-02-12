a51e757f807c29996125bb640c041d9b9e3ccd7b
Shinni <69644134+ShinniUwU@users.noreply.github.com>
2022-02-12 17:50:53 +0200

    Add files via upload

---
+++ listing 2/listing 2.1/listing 2.1.js
@@ -0,0 +1,9 @@
+var text,name
+text ="Haide da se zapoznaem! Kak se kazvate?"
+name=prompt(text)
+if(name=="") {
+    document.write("vie ne se predstavihte")
+}
+else {
+document.write("Priqtno mi e da se zapoznaem, "+name+"!")
+}
