e38613fe31c7f23306910e1f3bb68a69ba1f2538
Shinni <69644134+ShinniUwU@users.noreply.github.com>
2022-02-12 16:45:43 +0200

    Update listing 2.1.js

--- listing 2/listing 2.1/listing 2.1.js
+++ listing 2/listing 2.1/listing 2.1.js
@@ -4,7 +4,6 @@ name=prompt(text)
 if(name=="") {
     document.write("vie ne se predstavihte")
 }
-// the code below this comment isnt listed in the book so I thought I should make it a lil bit cooler
 else {
-    document.write("priqtno mi e da se zapoznaem " + name)
+document.write("Priqtno mi e da se zapoznaem, "+name+"!")
 }
