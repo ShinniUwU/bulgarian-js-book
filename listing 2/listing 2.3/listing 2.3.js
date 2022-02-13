57a6700235e9f30ca969e072614b119f7574e14c
Shinni <69644134+ShinniUwU@users.noreply.github.com>
2022-02-13 15:28:27 +0200

    Add files via upload

---
+++ listing 2/listing 2.3/listing 2.3.js
@@ -0,0 +1,29 @@
+// skript s vlojeni uslovni operatori
+// definirane na funkciqta:
+function f(x) {
+    // ako x<0
+    if (x <0) {
+    return 0
+}
+// ako x>= 0
+else {
+// ako x<2
+if(x<2){
+    return x*x 
+}
+
+// ako x>=2
+else {
+    // ako x<5
+    if(x<5) {
+        return 6-x
+    }
+
+// ako x>=5
+else {
+    return 1
+}
+}
+}
+}
+
