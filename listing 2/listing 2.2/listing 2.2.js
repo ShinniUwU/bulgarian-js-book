688878761c54208abdb3716eed3c83237a0158d2
Shinni <69644134+ShinniUwU@users.noreply.github.com>
2022-02-13 14:19:56 +0200

    Add files via upload

---
+++ listing 2/listing 2.2/listing 2.2.js
@@ -0,0 +1,8 @@
+var text,name,msg
+text="Haide da se zapoznaem! kak se kazvate Kak se kazvate?"
+msg="Vie ne se predstavihte"
+name=prompt(text)
+if(name!="") {
+    msg="priqtno mi e da se zapoznaem, "+name+"!"
+}
+document.write(msg)
